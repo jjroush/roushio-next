b41446047d38ce055f1fbd7ac03757e2511ebb3c
jjroush <jacobroushmail@gmail.com>
2020-12-16 23:37:29 -0600

    test commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { AboutFlex } from "../components/home/about-flex";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { AboutFlex } from '../components/home/about-flex';
 
 export default function Home() {
   return (
@@ -9,16 +9,14 @@ export default function Home() {
         <title>roush.io</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>{"Jacob Roush"}</h1>
+      <h1>{'Jacob Roush'}</h1>
       <h2>
         {
           "I'm a software engineer who is improving the online retail experience @ Hy-Vee."
         }
       </h2>
       <span>
-        <h3>
-          {"When I'm not slinging code at work or for fun; I like to brew beer"}
-        </h3>
+        <h3>{'👀'}</h3>
       </span>
       <AboutFlex />
     </>
