a175e14037b0f6a0cf4adb1629a5ee379fbc2e3b
jjroush <27168277+jjroush@users.noreply.github.com>
2024-12-11 22:37:16 -0600

    progress

--- components/home/about-flex.js
+++ components/home/about-flex.js
@@ -8,7 +8,7 @@ const devYears = () => new Date().getFullYear() - 2016;
 export const AboutFlex = () => (
   <div className={styles.flexContainer}>
     <div style={{flexBasis: 0, flexGrow: 1}}>
-      <p>{`I'm a Software Engineer Team Lead at Pella Corp. Fullstack JavaScript is my expertise, but enjoy learning new technologies to best solve new business problems.`}</p>
+      <p>{`I'm a Software Engineering Team Lead at Pella Corp. Fullstack JavaScript is my expertise, but enjoy learning new technologies to best solve new business problems.`}</p>
         <p>{`From time to time I like to share those learnings here.`}</p>
     </div>
     <div style={{flexBasis: 0, flexGrow: 1}}>
